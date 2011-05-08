34e097f8e20241786d4ca6e378be30b8380b974a
Oliver Kahrmann <oliverkahrmann@oliversmacbookpro.fritz.box>
2011-05-08 13:00:45 +0200

    Initial Commit

---
+++ DXCompress/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DXCompress
+//
+//  Created by Oliver Kahrmann on 08.05.11.
+//  Copyright 2011 DHELIXSoft. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
